431baeb30455394e5a211dc004d66d5dbb7ec445
niklas22005 <142879325+niklas22005@users.noreply.github.com>
2023-10-30 19:45:42 +0200

    Tein int asti

--- src/lucky7.java
+++ src/lucky7.java
@@ -15,39 +15,7 @@ public class lucky7 {
            System.out.println("Anna rahamäärä");
            int money = Integer.parseInt(scanner.nextLine());
 
-           while(money > 0) {
-                   money--;
-                   nro1 = rand.nextInt(10) + 1;
-                   nro2 = rand.nextInt(10) + 1;
-                   nro3 = rand.nextInt(10) + 1;
-                   if(nro1 == 7 && nro2 == 7 && nro3 == 7) {
-                           money = money + 10;
-                           System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 10 rahaa" );
-                           System.out.println("Rahaa jäljellä " + money);
-                   }
-                   else if(nro1 == 7 && nro2 == 7 || nro2 == 7 && nro3 == 7 || nro1 == 7 && nro3 == 7) {
-                           money = money + 5;
-                           System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 5 rahaa");
-                           System.out.println("Rahaa jäljellä " + money);
-                   }
-                   else if(nro1 == 7 || nro2 == 7 || nro3 == 7) {
-                           money = money + 3;
-                           System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 3 rahaa");
-                           System.out.println("Rahaa jäljellä " + money);
-                   }else {
-                           System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Hävisit pelin");
-                           System.out.println("Rahaa jäljellä " + money);
-                   }
-                   System.out.println("Haluatko pelata uudestaan?");
-                   System.out.println("Jos haluat pelata, paina Enter");
-                   System.out.println("Jos et halua pelata, kirjoita e ja paina Enter");
-                   String vastaus = scanner.nextLine();
-                   if(vastaus.equals("e")){
-                           System.out.println("Kiitos peleistä. Sinulle jäi rahaa " + money + " euroa");
-                           System.exit(0);
-                   }
-           }
-           System.out.println("Rahasi loppuivat");
+
    }	
 		
 		
