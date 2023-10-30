54dee033ccc235ffe17483ed6a5892c795d6e3bf
niklas22005 <142879325+niklas22005@users.noreply.github.com>
2023-10-30 19:46:57 +0200

    lisäsin while loopin ja elset

--- src/lucky7.java
+++ src/lucky7.java
@@ -17,7 +17,30 @@ public class lucky7 {
 
 
    }	
-		
+	           while(money > 0) {
+                   money--;
+                   nro1 = rand.nextInt(10) + 1;
+                   nro2 = rand.nextInt(10) + 1;
+                   nro3 = rand.nextInt(10) + 1;
+                   if(nro1 == 7 && nro2 == 7 && nro3 == 7) {
+                           money = money + 10;
+                           System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 10 rahaa" );
+                           System.out.println("Rahaa jäljellä " + money);
+                   }
+                   else if(nro1 == 7 && nro2 == 7 || nro2 == 7 && nro3 == 7 || nro1 == 7 && nro3 == 7) {
+                           money = money + 5;
+                           System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 5 rahaa");
+                           System.out.println("Rahaa jäljellä " + money);
+                   }
+                   else if(nro1 == 7 || nro2 == 7 || nro3 == 7) {
+                           money = money + 3;
+                           System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 3 rahaa");
+                           System.out.println("Rahaa jäljellä " + money);
+                   }else {
+                           System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Hävisit pelin");
+                           System.out.println("Rahaa jäljellä " + money);
+                   }
+	
 		
 
 	}
